8d3638db6e5de59c998e3139a829d4210d974a3e
Naschenn <45387463+Naschenn@users.noreply.github.com>
2024-08-17 16:35:59 +0100

    Initial Menu Scripting
    Created buttons, added menu change functionality + button animations

---
+++ Assets/Scripts/menuController.cs
@@ -0,0 +1,108 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuController : MonoBehaviour
+{
+
+    GameObject mainMenu, savesMenu, settingsMenu, currentMenu, previousMenu, playButton, settingsButton, quitButton, backButtonSettings, backButtonSaves;
+
+    public Sprite[] clickAnimationSprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mainMenu = GameObject.Find("mainMenu");
+        settingsMenu = GameObject.Find("settingsMenu");
+        savesMenu = GameObject.Find("savesMenu");
+
+        playButton = GameObject.Find("playButton");
+        settingsButton = GameObject.Find("settingsButton");
+        quitButton = GameObject.Find("quitButton");
+        backButtonSettings = GameObject.Find("backButtonSettings");
+        backButtonSaves = GameObject.Find("backButtonSaves");
+
+        currentMenu = mainMenu;
+        settingsMenu.SetActive(false);
+        savesMenu.SetActive(false);
+
+    }
+
+    IEnumerator clickAnimate(GameObject clickedButton, GameObject menuToOpen) {
+
+        clickedButton.GetComponent<Button>().interactable = false;
+
+        for (int i = 0; i < clickAnimationSprites.Length; i++)
+        {
+
+            clickedButton.GetComponent<Image>().overrideSprite = clickAnimationSprites[i];
+            yield return new WaitForSeconds(0.02f);
+            Debug.Log("WAOW");
+
+        }
+
+        clickedButton.GetComponent<Button>().interactable = true;
+
+        changeMenu(menuToOpen);
+    }
+
+    void changeMenu(GameObject menuToOpen)
+    {
+
+        // Hide the current menu and store it in previous menu
+
+        currentMenu.SetActive(false);
+        previousMenu = currentMenu;
+
+
+        // Display new menu and save it as current menu
+
+        menuToOpen.SetActive(true);
+        currentMenu = menuToOpen;
+    }
+    public void clickPlay()
+    {
+
+        // Play animation
+
+        StartCoroutine(clickAnimate(playButton, savesMenu));
+    }
+    public void clickSettings() {
+
+        // Play animation
+
+        StartCoroutine(clickAnimate(settingsButton, settingsMenu));
+
+    }
+
+    public void clickBack() {
+
+        // Play animation
+        if(settingsMenu.activeSelf == true)
+        {
+            StartCoroutine(clickAnimate(backButtonSettings, mainMenu));
+        }
+        else
+        {
+            StartCoroutine(clickAnimate(backButtonSaves, mainMenu));
+        }
+
+
+    }
+
+    public void clickQuit() {
+
+
+        // Play animation
+
+        StartCoroutine(clickAnimate(quitButton, mainMenu));
+        Application.Quit();
+
+    }
+
+
+}
